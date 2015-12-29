299b32d389aaf802dcccfbe2b6158eac68cf146a
nii_kenichi <cm10030@gmail.com>
2015-12-30 04:13:11 +0900

    Implement tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Task;
+use Illuminate\Http\Request;
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +30,44 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function() {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    });
+
+    /**
+     * Create New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->failed()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+        return redirect('/');
+    });
 });
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Quickstart - Basic</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar Contents -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
